fb5df92c38fc91857e3bf5694f340ddce1425c2a
Tiuklin <tiuklin@hotmail.com>
2025-07-10 23:06:39 +1200

    fix: update API base URL to Azure

--- trustasitter-frontend/src/services/api.js
+++ trustasitter-frontend/src/services/api.js
@@ -2,7 +2,8 @@ import axios from "axios";
 
 // Create an axios instance with default config
 const api = axios.create({
-  baseURL: "http://localhost:3000/api", // Base URL for the API
+  // baseURL: "http://localhost:3000/api", // Base URL for the API
+  baseURL: "https://trustasitter-api.azurewebsites.net/api", // Base URL for the API
 });
 
 // Function to log in user
